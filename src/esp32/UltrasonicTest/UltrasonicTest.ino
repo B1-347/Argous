707a88e7aefd857c6a7f52196bb9ca99add96763
B1 <101814341@student.swin.edu.au>
2022-09-16 14:54:16 +1000

    UPDATE: UltrasonicTest.ino
    Added comments to the code for ultrasonic sensor.

--- src/esp32/UltrasonicTest/UltrasonicTest.ino
+++ src/esp32/UltrasonicTest/UltrasonicTest.ino
@@ -6,10 +6,15 @@
  * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-ultrasonic-sensor
  */
 
+ //distance to an object = ((speed of sound in the air)*time)/2
+ //speed of sound in the air at 20ºC = 343m/s = 34300cm/s = 0.0343cm/us
+ //a degree to the surface that is greater 15 degrees will not be read
+
 #define TRIG_PIN 25 // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
 #define ECHO_PIN 26 // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
 
 float duration_us, distance_cm;
+int distDroneCentre = 0;
 
 void setup() {
   // begin serial port
@@ -30,12 +35,13 @@ void loop() {
   // measure duration of pulse from ECHO pin
   duration_us = pulseIn(ECHO_PIN, HIGH);
 
-  // calculate the distance
-  distance_cm = 0.017 * duration_us;
+  //calculate the distance 
+  distance_cm = (0.017 * duration_us)+ distDroneCentre;
 
   // print the value to Serial Monitor
   //Serial.print("distance: ");
-  Serial.println(min((double)distance_cm,400.0));
+  Serial.println((double)distance_cm);
+  //Serial.println(min((double)distance_cm,400.0));
   //Serial.println(" cm");
 
   delay(500);
